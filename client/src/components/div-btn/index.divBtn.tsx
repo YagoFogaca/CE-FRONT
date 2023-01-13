ba98d2eb295a8fd114081f65e379dd16877b31e3
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-13 16:27:33 -0300

    feat: implementação do type do parametro da função DivBtn

--- client/src/components/div-btn/index.divBtn.tsx
+++ client/src/components/div-btn/index.divBtn.tsx
@@ -1,12 +1,5 @@
+import { PDivBtn } from '../../utils/types/props.types';
 import * as C from './styled.divBtn';
-
-type PDivBtn = {
-  setView: (value: {
-    estoque: boolean;
-    saida: boolean;
-    entrada: boolean;
-  }) => void;
-};
 
 export function DivBtn({ setView }: PDivBtn) {
   return (
