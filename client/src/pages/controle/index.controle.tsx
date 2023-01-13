936f0aac9e004f210dece8cb1080cda0e86f3d45
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-13 18:41:17 -0300

    fix: renomeação do compomente

--- client/src/pages/controle/index.controle.tsx
+++ client/src/pages/controle/index.controle.tsx
@@ -2,7 +2,7 @@ import * as C from './style.controle';
 import { Main } from '../../components-styled/main/index.main';
 import { useState } from 'react';
 import { DivBtn } from '../../components/div-btn/index.divBtn';
-import { Table } from '../../components/table/index.table';
+import { Table } from '../../components/table-estoque/index.table';
 
 export function ControlePage() {
   const [view, setView] = useState({
