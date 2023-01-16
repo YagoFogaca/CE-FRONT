75d54e06fa6d39b6c28193cc70835ec3a12292cc
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-16 19:25:44 -0300

    feat: criação do estilo global do btn

---
+++ client/src/components-styled/btn/index.btn.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const BtnS = styled.button`
+  max-width: 100px;
+  width: 45%;
+  font-size: 1.8rem;
+  color: #fefefe;
+  padding: 0.3rem 0.5rem;
+  background-color: #5485bb;
+  border: 2px solid #0f0f0f;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  cursor: pointer;
+  opacity: 0.9;
+  transition: 0.3s;
+  @media (max-width: 425px) {
+    font-size: 1.4rem;
+    width: 30%;
+  }
+
+  &:hover {
+    opacity: 1;
+  }
+`;
