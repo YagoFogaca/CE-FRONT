82b39bb5b2597d968d5a5ad013034ba5137cefb3
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-23 18:08:08 -0300

    feat: criação e implementação da home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ControlPage } from './pages/control/index.control';
+import { Homepage } from './pages/homepage/index.homepage';
 import { LoginPage } from './pages/login/index.login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/homepage" element={<Homepage />} />
         <Route path="/controle-estoque" element={<ControlPage />} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/homepage/style.homege.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SectionHomepageS = styled.section`
+  width: 100%;
+  height: 100%;
+  margin-top: 3rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
