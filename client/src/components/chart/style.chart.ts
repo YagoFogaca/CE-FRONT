d89566edbb38c1336e8a2a394217ab68ff3619f2
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-24 08:53:51 -0300

    style: organização css Chart

--- client/src/components/chart/style.chart.ts
+++ client/src/components/chart/style.chart.ts
@@ -5,20 +5,19 @@ export const SectionGraficos = styled.section`
   height: 90%;
   padding: 1rem;
 
-  .chart-entry {
-    min-width: 450px !important;
-    display: flex;
-    justify-content: center;
-
-    tspan {
-      font-size: 12px;
-    }
-  }
-
   .row {
     overflow: auto;
     background-color: #f5f5f5;
     border-radius: 10px;
     box-shadow: 0px 1px 5px #000;
+  }
+
+  .chart-entry {
+    min-width: 450px !important;
+    display: flex;
+    justify-content: center;
+    tspan {
+      font-size: 12px;
+    }
   }
 `;
