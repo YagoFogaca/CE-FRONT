11a6b440fd47d90209ddc2da62e47872d792f5df
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-25 20:22:21 -0300

    feat: implementação da seção de analise na homepage

--- client/src/pages/homepage/index.homepage.tsx
+++ client/src/pages/homepage/index.homepage.tsx
@@ -1,16 +1,29 @@
-import { Chart } from '../../components/chart/index.chart';
+import { useEffect, useState } from 'react';
 import { DivInfos } from '../../components/div-infos-homepage/index.divInfosHomepage';
 import { Header } from '../../components/header/index.header';
 import { SectionInfos } from '../../components/section-infos-homepage/index.SectionHomepage';
+import { api } from '../../utils/api/api';
+import { ISuppliesEntrys } from '../../utils/interfaces/useState.interface';
 import * as C from './style.homege';
 
 export function Homepage() {
+  const [supply, setSupply] = useState<ISuppliesEntrys[]>([]);
+
+  const findSupply = async () => {
+    const suppleis = await api.findAllSpply();
+    setSupply(suppleis);
+  };
+
+  useEffect(() => {
+    findSupply();
+  }, []);
+
   return (
     <>
       <Header />
       <C.SectionHomepageS>
         <DivInfos />
-        <SectionInfos />
+        <SectionInfos supplies={supply} />
       </C.SectionHomepageS>
     </>
   );
