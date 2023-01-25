bd46547ff86191bc792790f0255b15ae1e697c8d
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-25 20:21:32 -0300

    feat: criação da seção para analise da entradas

--- client/src/components/section-infos-homepage/index.SectionHomepage.tsx
+++ client/src/components/section-infos-homepage/index.SectionHomepage.tsx
@@ -1,10 +1,69 @@
+import { useState } from 'react';
 import * as C from './style.SectionHomepage';
+import * as T from '../../components-styled/table-styled/index.table';
 import { FormSectionInfos } from '../forms/index.SectionsInfos';
+import { Chart } from '../chart/index.chart';
+import { FormSectionInfosFilter } from '../../utils/types/props.types';
+import {
+  ISuppliesEntrys,
+  PropsSuppliesEntrys,
+} from '../../utils/interfaces/useState.interface';
 
-export function SectionInfos() {
+export function SectionInfos({ supplies }: PropsSuppliesEntrys) {
+  const [filter, setFilter] = useState<FormSectionInfosFilter>({
+    nome: '',
+    quant: 0,
+  });
+
+  const CheckFilter = (supply: ISuppliesEntrys) => {
+    return (
+      supply.nome.toLowerCase().includes(filter.nome.toLowerCase()) &&
+      supply.quant_estoque > 0
+    );
+  };
+  const suppliesFilter = supplies.filter(CheckFilter);
+
   return (
     <C.SectionInfos>
-      <FormSectionInfos />
+      <FormSectionInfos setFilter={setFilter} />
+      {filter.nome ? (
+        <>
+          <C.SectionInfosDiv>
+            <T.TableS>
+              <T.TrS>
+                <T.ThS>Nome</T.ThS>
+                <T.ThS>Qtde</T.ThS>
+                <T.ThS>Data</T.ThS>
+              </T.TrS>
+              {suppliesFilter.map((insumo, index) => {
+                return (
+                  <T.TrS bg={index % 2 === 0 ? true : false} key={index}>
+                    <T.TdS widthP displayP>
+                      {insumo.id}
+                    </T.TdS>
+                    <T.TdS>{insumo.nome}</T.TdS>
+                    <T.TdS>{insumo.quant_estoque}</T.TdS>
+                    <T.TdS>{insumo.unidade.toUpperCase()}</T.TdS>
+                    <T.TdS displayP>
+                      {insumo.ativo ? 'ATIVO' : 'OBSOLETO'}
+                    </T.TdS>
+                  </T.TrS>
+                );
+              })}
+            </T.TableS>
+          </C.SectionInfosDiv>
+          {/* <Chart
+            id={'entradas-insumo'}
+            colors={['#177958']}
+            categories={categories}
+            text={'Entradas de insumos'}
+            name={'Qtde de Entrada'}
+            data={data}
+          /> */}
+        </>
+      ) : (
+        <></>
+      )}
     </C.SectionInfos>
   );
 }
