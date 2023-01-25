940d0172affc242d8e06d5bb958893f05dba574c
Yago Fogaça <yagofogaca20@gmail.com>
2023-01-25 14:07:33 -0300

    feat: desagrupação dos compomentes da página homepage

--- client/src/pages/homepage/index.homepage.tsx
+++ client/src/pages/homepage/index.homepage.tsx
@@ -5,45 +5,12 @@ import { SectionInfos } from '../../components/section-infos-homepage/index.Sect
 import * as C from './style.homege';
 
 export function Homepage() {
-  const categories = [
-    'Disco de Corte 4"',
-    'Disco de Desbaste 4"',
-    'Grafite',
-    'ABRAÇADEIRA DE NYLON',
-    'ABRAÇADEIRA METÁLICA 1"',
-    'ABRAÇADEIRA METÁLICA 1/2"',
-    'ANTIRESPINGO',
-    'ARAME MIG',
-    'LENTE 10',
-    'LENTE 12',
-    'LENTE 14',
-    'LENTE TRANSPARENTE',
-    'CONDUITE 3M',
-    'CONDUITE 4M',
-    'BICO DE CORTE 10',
-    'BICO DE CORTE 8',
-    'PORTA BICO M8',
-    'PORTA BICO M10',
-  ];
-
-  const data = [
-    30, 40, 45, 50, 49, 60, 2, 19, 30, 40, 45, 50, 49, 60, 2, 49, 60, 62,
-  ];
-
   return (
     <>
       <Header />
       <C.SectionHomepageS>
         <DivInfos />
         <SectionInfos />
-        {/* <Chart
-          id={'entradas-insumo'}
-          colors={['#177958']}
-          categories={categories}
-          text={'Entradas de insumos'}
-          name={'Qtde de Entrada'}
-          data={data}
-        /> */}
       </C.SectionHomepageS>
     </>
   );
