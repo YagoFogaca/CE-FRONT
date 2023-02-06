c9fb5a69ba0ce1a29585e2af2efbc4d126a15c26
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-06 14:23:48 -0300

    fix: atualização do compoements Section e Div-Section

--- client/src/pages/stock/index.stock.tsx
+++ client/src/pages/stock/index.stock.tsx
@@ -1,18 +1,19 @@
 import { Header } from '../../components/header/index.header';
 import { Main } from '../../components-styled/main/index.main';
+import { Section } from '../../components-styled/section/index.section';
+import { DivSectionS } from '../../components-styled/div-section/index.div-section';
 import { TablesStock } from '../../components/table-stock/index.table';
-import * as C from './style.stock';
 
 export function StockPage() {
   return (
     <>
       <Header />
       <Main>
-        <C.Section>
-          <C.DivSectionS>
+        <Section>
+          <DivSectionS>
             <TablesStock />
-          </C.DivSectionS>
-        </C.Section>
+          </DivSectionS>
+        </Section>
       </Main>
     </>
   );
