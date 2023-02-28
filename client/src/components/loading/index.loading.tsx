30c7835b14fb32f8c03e4ba962804e01b412373f
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-28 14:49:41 -0300

    feat: criação e implementação do compomente para loading

---
+++ client/src/components/loading/index.loading.tsx
@@ -0,0 +1,11 @@
+import './index.loading.css';
+
+export function Loading() {
+  return (
+    <>
+      <section id="section-loading">
+        <div id="loading"></div>
+      </section>
+    </>
+  );
+}
