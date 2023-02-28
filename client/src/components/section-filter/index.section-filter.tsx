64c432a1b9b2c46c76c389ff6d10fa141d1caf04
Yago Fogaça <yagofogaca20@gmail.com>
2023-02-28 10:00:56 -0300

    feat: components para filtro da aplicação

---
+++ client/src/components/section-filter/index.section-filter.tsx
@@ -0,0 +1,11 @@
+import './index.section-filter.css';
+
+export function SectionFilter() {
+  return (
+    <>
+      <section className="section-filter">
+        <input type="text" name="" id="" className="input-filter" />
+      </section>
+    </>
+  );
+}
