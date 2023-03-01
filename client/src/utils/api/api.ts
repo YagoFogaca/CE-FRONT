5bfb8d673d870cf7f42b14e3324ebc0315868206
Yago Fogaça <yagofogaca20@gmail.com>
2023-03-01 18:27:50 -0300

    fix: atualização da base url

--- client/src/utils/api/api.ts
+++ client/src/utils/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { IUserLogin } from '../interfaces/index.api';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://ce-api-production.up.railway.app';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 export class Api {
