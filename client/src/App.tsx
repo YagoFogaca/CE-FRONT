e084e33409f81f46e4cd5510c0c79126a26609e1
Yago Fogaça <yagofogaca20@gmail.com>
2023-03-01 10:52:35 -0300

    feat: implementação do component Auth para controle de acesso e autenticação das rotas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppRoutes } from './app.routes';
-import { Header } from './components/header/index.header';
 import { LoginPage } from './pages/login/index.login';
-import { StockPage } from './pages/stock/index.stock';
 import { Auth } from './utils/api/interceptors.api';
 
 export function App() {
   return (
     <BrowserRouter>
-      {/* <Auth /> */}
+      <Auth />
 
       <Routes>
         <Route path="/" element={<LoginPage />} />
