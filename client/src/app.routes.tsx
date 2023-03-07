6196ab2030f3d5d00a3d5fc0861a5dbf61a96d2b
Yago Fogaça <yagofogaca20@gmail.com>
2023-03-07 08:11:49 -0300

    feat: criação da página home, apenas para uso do context

--- client/src/app.routes.tsx
+++ client/src/app.routes.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/header/index.header';
-import { EntryPage } from './pages/entry/index.entry';
+import { HomePage } from './pages/home/index.home';
 import { StockPage } from './pages/stock/index.stock';
+import { EntryPage } from './pages/entry/index.entry';
 import { SupplyPage } from './pages/supply/index.supply';
 
 export function AppRoutes() {
@@ -9,6 +10,7 @@ export function AppRoutes() {
     <>
       <Header />
       <Routes>
+        <Route path="home" element={<HomePage />} />
         <Route path="estoque" element={<StockPage />} />
         <Route path="entry" element={<EntryPage />} />
         <Route path="supply/:id" element={<SupplyPage />} />
---
+++ client/src/pages/home/index.home.tsx
@@ -0,0 +1,5 @@
+import './index.style.home.css';
+
+export function HomePage() {
+  return <h1>Essa é a home</h1>;
+}
